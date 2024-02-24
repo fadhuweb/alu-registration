cbbf69b68e9db0da088d1285ee8894d4ae01f11a
fadhuweb <f.abdulazee@alustudent.com>
2024-02-24 10:14:22 -0800

    Gsc 2

---
+++ move-to-directory.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Move all 4 files created in question 1 to a directory with the specified name
+mkdir negpod_id-q1
+mv main.sh Students-list_1023.txt select-emails.sh student-emails.txt negpod_id-q1/
+echo "Files moved to negpod_id-q1 directory successfully!"
+
