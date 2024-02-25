723f1245e92df7c9f0548a93a4b3cbb70dba6882
Ganzadidier <d.ganza@alustudent.com>
2024-02-25 10:01:53 -0800

    backup file updated

--- backup-Negpod_ID.sh
+++ backup-Negpod_ID.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# Backup directory to remote server
-rsync -avz -e "sshpass -p '328d3b338a4ced526c9a' ssh -o StrictHostKeyChecking=no" negpod_id-q1/ 64293e56bc62@64293e56bc62.3a2627c1.alu-cod.online:/summative/1023-2024j
-echo "Directory backed up to remote server successfully!"
+# Details about the remote server
+host="64293e56bc62.3a2627c1.alu-cod.online"
+username="64293e56bc62"
+password="328d3b338a4ced526c9a"
+backup_location="/summative/1023-2024j"
 
+# Directory to be backed up
+directory_to_backup="negpod_id-q1"
+
+# Backup the directory to the remote server
+scp -r "$directory_to_backup" "$username"@"$host":"$backup_location"
+
+echo "Directory $directory_to_backup backed up to $host:$backup_location successfully."
